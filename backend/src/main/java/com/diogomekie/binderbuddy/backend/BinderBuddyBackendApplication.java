fa918f169dd11f860711b838c957a9bd42c7cbaa
DiogoMekie <diogo.goncalves@codeforall.com>
2025-06-26 17:04:51 +0100

    Initial Angular frontend project setup

---
+++ backend/src/main/java/com/diogomekie/binderbuddy/backend/BinderBuddyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.diogomekie.binderbuddy.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BinderBuddyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BinderBuddyBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/diogomekie/binderbuddy/backend/BinderBuddyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diogomekie.binderbuddy.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BinderBuddyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
